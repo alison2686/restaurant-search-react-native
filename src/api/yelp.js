9954b8c8bd4c8076de546e76e4ff92e07a76ce78
Alison Lee <alison2686@gmail.com>
2022-06-27 12:04:10 -0700

    add react-native-dotenv

---
+++ src/api/yelp.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { YELP_API_KEY } from '@env'
+// import { YellowBox } from 'react-native-web'
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`
+    }
+})
+
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,34 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from './components/SearchBar'
+import yelp from '../api/yelp'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
+  // state for resturant results
+  const [results, setResults] = useState([])
+  
+  // helper function to get yelpApi results
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+        params: {
+            limit: 50,
+            term,
+            location: 'san francisco'
+        }
+    })
+    setResults(response.data.businesses)
+  }
 
   return (
     <View>
         <SearchBar 
             term={term} 
-            onTermChange={(newTerm) => setTerm(newTerm)}
-            onTermSubmit={() => console.log('term was submitted')}
+            onTermChange={setTerm}
+            onTermSubmit={searchApi}
         />
         <Text>Search Screen</Text>
-        <Text>{term}</Text>
+        <Text>We have found {results.length} results</Text>
     </View>
   )
 }
