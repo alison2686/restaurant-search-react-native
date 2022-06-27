e2d9597d5bfc79951e6a7d9dd5180c1974c35194
Alison Lee <alison2686@gmail.com>
2022-06-27 13:09:30 -0700

    add useEffecr for searchApi

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import { YELP_API_KEY } from '@env'
-// import { YellowBox } from 'react-native-web'
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from './components/SearchBar'
 import yelp from '../api/yelp'
@@ -7,27 +7,39 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('')
   // state for resturant results
   const [results, setResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
   
   // helper function to get yelpApi results
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-        params: {
-            limit: 50,
-            term,
-            location: 'san francisco'
-        }
-    })
-    setResults(response.data.businesses)
+  const searchApi = async (searchTerm) => {
+    console.log('Hi there')
+    try {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: searchTerm,
+                location: 'san francisco'
+            }
+        })
+        setResults(response.data.businesses)
+    } catch (err) {
+        setErrorMessage('Something went wrong')
+    }
   }
+
+  // Call searchAPI when component is first rendered. BAD CODE!
+//   searchApi('pasta')
+  useEffect(() => {
+    searchApi('pasta')
+  }, [])
 
   return (
     <View>
         <SearchBar 
             term={term} 
             onTermChange={setTerm}
-            onTermSubmit={searchApi}
+            onTermSubmit={() => searchApi(term)}
         />
-        <Text>Search Screen</Text>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {results.length} results</Text>
     </View>
   )
